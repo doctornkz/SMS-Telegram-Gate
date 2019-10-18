d097c4bd8d8cb5ba836563f2d646d591f717c97b
Oles Pisarenko <oles.pisarenko@olx.com>
2019-10-18 18:55:24 +0100

    Fix visibility

--- app/src/main/java/com/local/doctornkz/mobgate/MainActivity.java
+++ app/src/main/java/com/local/doctornkz/mobgate/MainActivity.java
@@ -26,11 +26,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
-        //Toolbar toolbar = findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
-
 
         requestPermission(Manifest.permission.RECEIVE_SMS, PERMISSIONS_REQUEST_RECEIVE_SMS);
 
